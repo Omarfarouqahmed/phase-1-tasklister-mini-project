d3c5f912837dada8707c568a856a02b89e55ecd9
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-06-25 14:42:34 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector ('form')
+  form.addEventListener('submit' , (event)=> {
+    event.preventDefault()
+  let newToDo = event.target.newtask.value
+  let user = event.target.user.value
+  let priority = event.target.priority.value
+  console.log (priority)
+  Todo (newTodo , user , priority)
+  console.log (newTodo, user)
+  form.reset()
+  })
+})
+function Todo(todo,user2,prioritY){
+  const li=document.createElement('li')
+  const btn=document.createElement('button')
+  const user=document.createElement('span')
+  btn.addEventListener('click',handleDelete)
+  li.addEventListener('mouseover',setColor)
+  btn.innerText='x'
+  li.innerHTML=`${todo} ${user2} ${prioritY} 
+   `
+   
+  li.appendChild(btn)
+  
+  
+  function setColor(priority){
+    if(priority==='Urgent'){
+     priority=document.getElementsByTagName('li').style.color = "#cccfffggg";
+    }else if(priority==='Less Urgent'){
+     priority= document.getElementsByTagName('li').style.color = "green";
+    }
+    return priority
+  }
+  setColor()
+  document.querySelector('#tasks').appendChild(li)
+  }
+  
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+
